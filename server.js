d2bfbadc8523bed5efd5371cf4133d9906a67371
Bankapur Ananthasetty Raghavendra <ba.raghu@gmail.com>
2017-09-22 11:30:10 +0530

    Making the code work to display it in html

--- server.js
+++ server.js
@@ -1,37 +1,45 @@
 const express = require("express");
 const hbs = require("hbs");
-var https = require('https');
+const _ = require('lodash')
+var https = require("https");
 
 const port = process.env.PORT || 3000;
 var app = express();
 
-hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerPartials(__dirname + "/views/partials");
 
 app.set("view engine", "hbs");
 
+hbs.registerHelper("ifbool", value => {
+  if (_.isBoolean(value)) {
+    return true;
+  }
+  return false;
+});
+
 app.get("/", (req, res) => {
-    res.render('home.hbs',{
-        pagetitle:'Manage your todo list'
-    });
+  res.render("home.hbs", {
+    pagetitle: "Manage your todo list"
+  });
 });
 
-app.get("/todos",(req,res)=>{
-    res.render('todos.hbs',{
-        pagetitle:"Todo list",
-        todos:[
-        {
-            "_id": "59c2137d36ac4900129f556c",
-            "text": "from postmane",
-            "__v": 0,
-            "completedAt": null,
-            "completed": false
-        }
+app.get("/todos", (req, res) => {
+  res.render("todos.hbs", {
+    pagetitle: "Todo list",
+    todos: [
+      {
+        _id: "59c2137d36ac4900129f556c",
+        text: "from postmane",
+        __v: 0,
+        completedAt: null,
+        completed: true
+      }
     ]
-    })
+  });
 });
 
-app.listen(port, ()=>{
-    console.log(`Server is up on port ${port}`);
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
 });
 
 module.exports.app = app;
