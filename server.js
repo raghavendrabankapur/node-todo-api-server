67a5b8a93b93f8779d790e7bffca4a7fac937e17
Bankapur Ananthasetty Raghavendra <ba.raghu@gmail.com>
2017-09-22 15:05:09 +0530

    Including css into the html

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + "/views/partials");
+//app.use(express.static(__dirname + '/public'));
 
 app.set("view engine", "hbs");
 
